a6c81714467d31f79dd4067cd4126440f19eed5a
Rosalie Wessels <rosaliew@rosalies-mbp.hsd1.ca.comcast.net>
2018-04-29 10:12:31 -0700

    Added all of the homework input fields

--- Studify/Message.swift
+++ Studify/Message.swift
@@ -4,12 +4,19 @@
 //
 //  Created by Rosalie Wessels on 13/04/2018.
 
+import UIKit
+
 class Message {
     
     //TODO: Messages need a messageBody and a sender variable
     
     var title: String = " "
     var teacher: String = " "
+    var className: String = " "
+    var assignedDate: Date = Date.init(timeIntervalSinceNow: 0)
+    var dueDate: Date = Date.init(timeIntervalSinceNow: 0)
+    var description: String = " "
+    var other: String = " "
     var done: Int = 0
     
 }
